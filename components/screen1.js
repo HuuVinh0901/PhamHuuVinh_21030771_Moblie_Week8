6831391f084538673441ea43344b7a44b3b791ae
Vinh <phamhuuvinh0901@gmail.com>
2024-10-27 08:18:39 +0700

    Câu 2e

--- components/screen1.js
+++ components/screen1.js
@@ -1,25 +1,78 @@
 import { View, Text, Image, TextInput, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import { FlatList } from 'react-native-web'
 
 const category = [
-    { name: 'Resort', image: 'resort.png' },
-    { name: 'Homestay', image: 'homestay.png' },
-    { name: 'Hotel', image: 'hotel.png' },
-    { name: 'Lodge', image: 'lodge.png' },
-    { name: 'Villa', image: 'villa.png' },
-    { name: 'Apartment', image: 'apartment.png' },
-    { name: 'Hostel', image: 'hostel.png' },
-    { name: 'See all', image: 'seeall.png' },
+    { id: 1, name: 'Resort', image: require('../assets/data/resort.png') },
+    { id: 2, name: 'Homestay', image: require('../assets/data/homestay.png') },
+    { id: 3, name: 'Hotel', image: require('../assets/data/hotel.png') },
+    { id: 4, name: 'Lodge', image: require('../assets/data/lodge.png') },
+    { id: 5, name: 'Villa', image: require('../assets/data/villa.png') },
+    { id: 6, name: 'Apartment', image: require('../assets/data/apartment.png') },
+    { id: 7, name: 'Hostel', image: require('../assets/data/hostel.png') },
+    { id: 8, name: 'See all', image: require('../assets/data/seeall.png') },
 ]
 const location = [
-    { image: 'photo1.png' },
-    { image: 'photo2.png' },
-    { image: 'photo3.png' },
-    { image: 'photo4.png' },
-    { image: 'photo5.png' },
-    { image: 'photo1.png' },
+    { id: 1, image: require('../assets/data/photo1.png') },
+    { id: 2, image: require('../assets/data/photo2.png') },
+    { id: 3, image: require('../assets/data/photo3.png') },
+    { id: 4, image: require('../assets/data/photo4.png') },
+    { id: 5, image: require('../assets/data/photo5.png') },
+    { id: 6, image: require('../assets/data/photo1.png') },
 ]
-const screen1 = () => {
+
+const Screen1 = () => {
+    const imageMap = {
+        "resort.png": require('../assets/data/resort.png'),
+        "homestay.png": require('../assets/data/homestay.png'),
+        "hotel.png": require('../assets/data/hotel.png'),
+        "lodge.png": require('../assets/data/lodge.png'),
+        "villa.png": require('../assets/data/villa.png'),
+        "apartment.png": require('../assets/data/apartment.png'),
+        "hostel.png": require('../assets/data/hostel.png'),
+        "seeall.png": require('../assets/data/seeall.png')
+    }
+    const [cate, setCate] = useState()
+    const [loca, setLoca] = useState()
+    useEffect(() => {
+        fetchDataCate();
+        fetchDataLocation();
+    }, [])
+    const fetchDataCate = async () => {
+        try {
+            const respone = await fetch('https://671a7948acf9aa94f6aac459.mockapi.io/api/ThiGK/category')
+            const jsonDataCate = await respone.json()
+            setCate(jsonDataCate)
+        } catch (error) {
+            console.error('Lỗi', error)
+        }
+    }
+    const fetchDataLocation = async () => {
+        try {
+            const respone = await fetch('https://671a7948acf9aa94f6aac459.mockapi.io/api/ThiGK/location')
+            const jsonDataLoca = await respone.json()
+            setLoca(jsonDataLoca)
+        } catch (error) {
+            console.error('Lỗi', error)
+        }
+    }
+    const renderCate = ({ item }) => {
+        return (
+            <View style={{ gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+                <TouchableOpacity><Image source={imageMap[item.image]} /></TouchableOpacity>
+                <Text style={{ alignItems: 'center', justifyContent: 'center' }}>{item.name}</Text>
+            </View>
+        )
+    };
+    const renderLoca = ({ item }) => {
+        return (
+            <View style={{ gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+                <TouchableOpacity><Image source={imageMap[item.image]} /></TouchableOpacity>
+                <Text style={{ alignItems: 'center', justifyContent: 'center' }}>{item.name}</Text>
+            </View>
+        )
+    };
     return (
         <ScrollView>
 
@@ -45,13 +98,64 @@ const screen1 = () => {
                     </View>
                 </View>
             </View>
-            <View style={{ height: 400 }}>
-                <View style={{ flexDirection: 'row',marginHorizontal:25,marginVertical:10 }}>
-                    <View style={{width:'50%'}}><Text style={{fontSize:20}}>Category</Text></View>
+            <ScrollView style={{ height: 600 }}>
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, marginVertical: 10 }}>
+                    <View style={{ width: '50%' }}><Text style={{ fontSize: 20 }}>Category</Text></View>
 
-                    <View  style={{width:'50%',alignItems:'flex-end'}}><Image style={{ height: 30, width: 30 }} source={require('../assets/data/3gach.png')} /></View>
+                    <View style={{ width: '50%', alignItems: 'flex-end' }}><Image style={{ height: 30, width: 30 }} source={require('../assets/data/3gach.png')} /></View>
                 </View>
-            </View>
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, gap: 10 }}>
+                    {category.slice(0, 4).map((item) => (
+                        <View style={{ gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+                            <TouchableOpacity><Image source={item.image} /></TouchableOpacity>
+                            <Text style={{ alignItems: 'center', justifyContent: 'center' }}>{item.name}</Text>
+                        </View>
+                    ))}
+
+                </View>
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, gap: 10, marginTop: 10 }}>
+                    {category.slice(4, 8).map((item) => (
+                        <View style={{ gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+                            <TouchableOpacity><Image source={item.image} /></TouchableOpacity>
+                            <Text style={{ alignItems: 'center', justifyContent: 'center' }}>{item.name}</Text>
+                        </View>
+                    ))}
+
+                </View>
+
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, marginVertical: 10 }}>
+                    <View style={{ width: '70%' }}><Text style={{ fontSize: 20 }}>Popular destination</Text></View>
+
+                    <View style={{ width: '30%', alignItems: 'flex-end' }}><Image style={{ height: 30, width: 30 }} source={require('../assets/data/3gach.png')} /></View>
+                </View>
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, gap: 10 }}>
+                    {location.slice(0, 3).map((item) => (
+                        <View style={{ gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+                            <TouchableOpacity><Image style={{ width: 100, height: 80, borderRadius: 10, marginLeft: 5 }} source={item.image} /></TouchableOpacity>
+                            <Text style={{ alignItems: 'center', justifyContent: 'center' }}>{item.name}</Text>
+                        </View>
+                    ))}
+
+                </View>
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, marginVertical: 10 }}>
+                    <View style={{ width: '70%' }}><Text style={{ fontSize: 20 }}>Recommended</Text></View>
+                </View>
+                <View style={{ flexDirection: 'row', marginHorizontal: 25, gap: 10 }}>
+                    {location.slice(3, 5).map((item) => (
+                        <View style={{ gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+                            <TouchableOpacity><Image style={{ width: 160, height: 140, borderRadius: 10, marginLeft: 5 }} source={item.image} /></TouchableOpacity>
+                            <Text style={{ alignItems: 'center', justifyContent: 'center' }}>{item.name}</Text>
+                        </View>
+                    ))}
+
+                </View>
+
+
+
+
+
+
+            </ScrollView>
             <View style={{ backgroundColor: '#5e45c4' }}>
                 <View style={{ flexDirection: 'row', gap: 40, marginVertical: 30, marginHorizontal: 50 }}>
                     <Image style={{ height: 40, width: 40 }} source={require('../assets/data/homeicon.png')} />
@@ -60,9 +164,15 @@ const screen1 = () => {
                     <Image style={{ height: 40, width: 40 }} source={require('../assets/data/profileicon.png')} />
                 </View>
             </View>
+            <FlatList data={cate} keyExtractor={(item) => item.id.toString()}
+                renderItem={renderCate}
+            >
 
-        </ScrollView>
+            </FlatList>
+        </ScrollView >
+
+
     )
 }
 
-export default screen1
+export default Screen1
